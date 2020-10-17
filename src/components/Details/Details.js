331011b72038be9f5724cb4d2c567ff85481afdc
DavydovDenys <denvanrain@gmail.com>
2020-10-17 23:18:55 +0300

    refactoring routing in Calendar

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,28 +1,45 @@
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
+import {useHistory} from 'react-router-dom';
+import YouTube from 'react-youtube';
 import './Details.css'
+import UseLaunches from "../UseLaunches/UseLaunches";
 
-const Details = (props) => (
-  <main className="details">
-    <div className="container">
-      <div className="details-row">
-        <div className="details-image">
-          <img src="https://images2.imgbox.com/3c/0e/T8iJcSN3_o.png" alt=""/>
+
+const Details = (props) => {
+  const history = useHistory();
+
+  const [launch, setLaunch] = useState(null);
+
+  const {getLaunch} = UseLaunches();
+  useEffect(() => {
+    setLaunch(getLaunch(props.match.params.id));
+  });
+
+  console.log(launch)
+
+  if (!launch) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+
+    <main className="details">
+      <div className="container">
+        <div className="details-row">
+          <div className="details-image">
+            <img src={launch.links.patch.small} alt={launch.name}/>
+          </div>
+          <div className="details-content">
+            <p className="details-description">{launch.details}</p>
+          </div>
         </div>
-        <div className="details-content">
-          <p className="details-description">Engine failure at 33 seconds and loss of vehicle</p>
+        <div>
+          <YouTube className="details-youtube" autoplay videoId={launch.links.youtube_id}/>
         </div>
       </div>
-      <div>
-        <iframe className="details-youtube" width="560" height="315" src="https://www.youtube.com/embed/dLQ2tZEH6G0"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen></iframe>
-      </div>
-    </div>
-    <a href="calendar.html" className="button button-back">go back</a>
-  </main>
-
-);
+      <a onClick={history.goBack} className="button button-back">go back</a>
+    </main>
+  );
+}
 
 export default Details;
